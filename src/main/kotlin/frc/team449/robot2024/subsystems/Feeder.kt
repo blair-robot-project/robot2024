300ca935122507ac50e0ac8d187f9b914a744199
Michael Spears <mts18142027@gmail.com>
2024-01-25 17:14:08 -0500

    Added to Feeder

--- src/main/kotlin/frc/team449/robot2024/subsystems/Feeder.kt
+++ src/main/kotlin/frc/team449/robot2024/subsystems/Feeder.kt
@@ -1,4 +1,51 @@
 package frc.team449.robot2024.subsystems
-class Feeder {
+import edu.wpi.first.util.sendable.SendableBuilder
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team449.robot2024.constants.subsystem.FeederConstants
+import frc.team449.robot2024.constants.subsystem.IntakeConstants
+import frc.team449.system.encoder.NEOEncoder
+import frc.team449.system.motor.WrappedMotor
+import frc.team449.system.motor.createSparkMax
+class Feeder(
+  private val motor: WrappedMotor
+) : SubsystemBase() {
+  fun feed(): Command {
+    return this.runOnce {
+      motor.setVoltage(IntakeConstants.INTAKE_VOLTAGE)
+    }
+  }
 
+  fun reverse(): Command {
+    return this.runOnce {
+      motor.setVoltage(IntakeConstants.REVERSE_VOLTAGE)
+    }
+  }
+
+  fun stop(): Command {
+    return this.runOnce {
+      motor.stopMotor()
+    }
+  }
+  override fun initSendable(builder: SendableBuilder) {
+    builder.publishConstString("1.0", "Feeder Motor Voltages")
+    builder.addDoubleProperty("1.1 Last Feeder Motor Voltage", { motor.lastVoltage }, null)
+  }
+  companion object {
+    fun createIntake(): Feeder {
+      val motor = createSparkMax(
+        "Feeder Motor",
+        FeederConstants.MOTOR_ID,
+        NEOEncoder.creator(
+          1.0,
+          1.0
+        ),
+        inverted = FeederConstants.INVERTED,
+        currentLimit = FeederConstants.CURRENT_LIM,
+        slaveSparks = mapOf(Pair(FeederConstants.FOLLOLWER_ID, FeederConstants.FOLLOWER_INV))
+      )
+
+      return Feeder(motor)
+    }
+  }
 }
