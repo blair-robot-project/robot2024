ab5752c213d8fefe8eec35a578e909573b3b9abd
blueberry <jpothen8@gmail.com>
2024-02-08 17:03:26 -0800

    shooter + feeder testing 02/07

--- src/main/kotlin/frc/team449/robot2024/constants/RobotConstants.kt
+++ src/main/kotlin/frc/team449/robot2024/constants/RobotConstants.kt
@@ -52,7 +52,7 @@ object RobotConstants {
 
   const val ALIGN_ROT_SPEED = 3 * PI / 2
 
-  val IR_CHANNEL = 15
+  val IR_CHANNEL = 9
 
   // Robot dimensions (INCLUDING BUMPERS)
   val ROBOT_WIDTH = Units.inchesToMeters(27.25 + 3.25 * 2)
