e637737a18793176c411f35f5526d23cc6c4f725
John Joseph <jpothen8@gmail.com>
2024-02-17 22:37:02 -0500

    delete generic "transform for pos 2"

--- src/main/kotlin/frc/team449/robot2024/auto/AutoUtil.kt
+++ src/main/kotlin/frc/team449/robot2024/auto/AutoUtil.kt
@@ -84,29 +84,6 @@ object AutoUtil {
     ) { RobotBase.isReal() }
   }
 
-  fun transformForPos2(pathGroup: MutableList<ChoreoTrajectory>): MutableList<ChoreoTrajectory> {
-    for (index in 0 until pathGroup.size) {
-      for (time in pathGroup[index].objectiveTimestamps) {
-        val currentMatrix = pathGroup[index].stateMap.get(time)
-
-        val newMatrix = MatBuilder.fill(
-          Nat.N2(),
-          Nat.N3(),
-          currentMatrix[0, 0],
-          FieldConstants.fieldWidth - currentMatrix[0, 1],
-          -currentMatrix[0, 2],
-          currentMatrix[1, 0],
-          -currentMatrix[1, 1],
-          -currentMatrix[1, 2]
-        )
-
-        pathGroup[index].stateMap.put(time, newMatrix)
-      }
-    }
-
-    return pathGroup
-  }
-
   fun transformForRed(pathGroup: MutableList<ChoreoTrajectory>): MutableList<ChoreoTrajectory> {
     for (index in 0 until pathGroup.size) {
       for (time in pathGroup[index].objectiveTimestamps) {
