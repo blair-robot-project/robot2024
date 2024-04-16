d9b1a8f812d0e0f349e5f7ceb18c0a9e3624b7a2
jpothen8 <jpothen8@gmail.com>
2024-04-16 08:05:32 -0400

    jiggle for less time

--- src/main/kotlin/frc/team449/robot2024/constants/subsystem/FeederConstants.kt
+++ src/main/kotlin/frc/team449/robot2024/constants/subsystem/FeederConstants.kt
@@ -10,9 +10,9 @@ object FeederConstants {
   const val INTAKE_VOLTAGE = 7.0
   const val SLOW_INTAKE_VOLTAGE = 3.85
   const val AUTO_SHOOT_INTAKE_VOLTAGE = 12.0
-  const val REVERSE_VOLTAGE = -2.35
+  const val REVERSE_VOLTAGE = -2.15
 
   const val BRAKE_MODE = false
 
-  const val CHECK_NOTE_IN_LOCATION_TIMEOUT_SECONDS = 2.0
+  const val CHECK_NOTE_IN_LOCATION_TIMEOUT_SECONDS = 1.35
 }
